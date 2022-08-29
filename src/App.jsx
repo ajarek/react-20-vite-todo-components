0083a6c79f9e077228e286e83d00915161e8bd29
ajarek <ajarek@poczta.onet.pl>
2022-08-30 00:26:31 +0200

    add info, btn-remove

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,78 @@
-import React from "react";
-import "./App.css";
-import { Form } from "./components/Form/Form";
-import { List } from "./components/List/List";
+import React from "react"
+import "./App.css"
+import {Button} from "./components/Button/Button"
+import { Form } from "./components/Form/Form"
+import { List } from "./components/List/List"
 export class App extends React.Component {
   state = {
     newTaskText: "",
-    tasks:[]
-  };
+    tasks:[],
+    infoItems:'',
+    arrIsCompleted:[]
+  }
   toggleItem = (id) => {
-    const newElements = this.state.tasks;
+    const newElements = this.state.tasks
     newElements.forEach((element) => {
       if (element.id === id) {
-        element.isCompleted = !element.isCompleted;
-        this.setState({ tasks: newElements });
+        element.isCompleted = !element.isCompleted
+        this.setState({ tasks: newElements })
       }
-    });
-  };
+    })
+  }
 
   editItem = (id) => {
-    const newElements = this.state.tasks;
+    const newElements = this.state.tasks
     newElements.forEach((element) => {
       if (element.id === id) {
-        this.setState({ newTaskText: element.text });
-        this.deleteItem(element.id);
+        this.setState({ newTaskText: element.text })
+        this.deleteItem(element.id)
       }
-    });
-  };
+    })
+  }
 
   deleteItem = (id) => {
-    const newElements = this.state.tasks.filter((element) => element.id != id);
+    const newElements = this.state.tasks.filter((element) => element.id != id)
 
-    this.setState({ tasks: newElements });
-  };
+    this.setState({ tasks: newElements })
+  }
 
   onNewTaskTextChange = (e) => {
-    this.setState({ newTaskText: e.target.value });
-  };
+    this.setState({ newTaskText: e.target.value })
+  }
 
   addNewTask = (e) => {
-    e.preventDefault();
+    e.preventDefault()
     if (!this.state.newTaskText) {
-      return;
+      return
     }
     const newTask = {
       id: String(Date.now()),
       text: this.state.newTaskText,
       isCompleted:false,
-    };
-    this.setState({ tasks: [newTask, ...this.state.tasks] });
-    this.state.newTaskText = "";
-  };
+    }
+    this.setState({ tasks: [newTask, ...this.state.tasks] })
+    this.state.newTaskText = ""
+  }
+  changeInfo=()=>{
+   this.state.infoItems=`${this.state.arrIsCompleted.length} of ${this.state.tasks.length} tasks done`
+  }
+ 
 //localStorage
 componentDidMount () {
   const tasksRaw = localStorage.getItem('tasks')
   const tasks = JSON.parse(tasksRaw) || []
   this.setState(() => ({ tasks }))
+ 
 }
   componentDidUpdate () {
     localStorage.setItem('tasks', JSON.stringify(this.state.tasks))
+    
 }
 
   render() {
+   
+    this.state.arrIsCompleted= this.state.tasks.filter(el=>el.isCompleted===true)
+    this.changeInfo()
     return (
       <div className="App">
         <div className="container">
@@ -86,9 +98,20 @@ componentDidMount () {
               </List>
             ))}
           </ul>
+          <div className="row-info">                   
+          <div className="info"
+          >{this.state.infoItems}
+            <span
+            style={{ width:`${this.state.arrIsCompleted.length/this.state.tasks.length*100}%`}}
+            ></span>
+            </div>
+          <Button
+          className={" btn-remove-checked"}
+          >Remove checked ❌</Button>
+          </div>
         </div>
       </div>
-    );
+    )
   }
 }
-export default App;
+export default App
